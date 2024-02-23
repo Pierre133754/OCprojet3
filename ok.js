29fcb6d2f2a7ab1c6a1eab7dce8269fefd3b0043
Big Man <froxyopro@yahoo.fr>
2024-02-23 23:00:32 +0100

    Filters done

--- ok.js
+++ ok.js
@@ -1,6 +1,14 @@
 import { 
-    createFigure
+    createFigure,
+    createFilters,
+    createBtn
 } from "./stuff.js";
 
 const Lworks = await fetch ("http://localhost:5678/api/works").then(Lworks => Lworks.json());
 createFigure(Lworks);
+
+const Lcat = await fetch ("http://localhost:5678/api/categories").then(Lcat => Lcat.json());
+createFilters(Lcat);
+
+createBtn(document.querySelectorAll(".filtres div"));
+
--- stuff.js
+++ stuff.js
@@ -1,5 +1,6 @@
 export function createFigure(meat) {
-    let figure = ''
+    document.querySelector(".gallery").innerHTML = "";
+    let figure = ``;
     for (let i = 0; i < meat.length; i++) {
         figure = `
         ${figure}
@@ -10,4 +11,38 @@ export function createFigure(meat) {
         `;
     }
     document.querySelector(".gallery").innerHTML = figure;
+}
+
+export function createFilters(meat) {
+    let dog = `<div class="activeF"><p>Tous</p></div>`;
+    for (let i = 0; i < meat.length; i++) {
+        dog =`
+        ${dog}
+        <div><p>${meat[i].name}</p></div>
+        `;
+    }
+    document.querySelector(".filtres").innerHTML = dog;
+}
+
+
+export async function createBtn(meat) {
+    const huh = await fetch ("http://localhost:5678/api/works").then(Lworks => Lworks.json());
+    for (let i = 0; i < meat.length; i++) {
+        if (meat[i].innerText === "Tous") {
+            meat[i].addEventListener("click", function () {
+                createFigure(huh);
+                document.querySelector(".activeF").classList.remove("activeF");
+                meat[i].classList.add("activeF");
+            });
+        } else {
+            meat[i].addEventListener("click", function () {
+                const buh = huh.filter(function (cookieMAN) {
+                    return cookieMAN.category.name === meat[i].innerText;
+                });
+                createFigure(buh);
+                document.querySelector(".activeF").classList.remove("activeF");
+                meat[i].classList.add("activeF");
+            });
+        }
+    }
 }
